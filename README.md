1add8962d78510e039a933f2aadfc1db73affc33
Raffaello Bonghi <raffaello@rnext.it>
2022-09-25 16:45:32 +0100

    Rearrange repository

--- README.md
+++ README.md
@@ -1,32 +1,18 @@
-# 🦕 nanosaur
-
-[![Discord](https://img.shields.io/discord/797461428646707211)](https://discord.gg/YvxjxEFPkb) [![GitHub Org's stars](https://img.shields.io/github/stars/rnanosaur?style=social)](https://github.com/rnanosaur) [![Twitter Follow](https://img.shields.io/twitter/follow/raffaello86?style=social)](https://twitter.com/raffaello86) [![robo.panther](https://img.shields.io/badge/Follow:-robo.panther-E4405F?style=social&logo=instagram)](https://www.instagram.com/robo.panther/)
-
-**nanosaur** The smallest [NVIDIA Jetson](https://developer.nvidia.com/buy-jetson) dinosaur robot, **open-source**, fully **3D printable**, based on [**ROS2**](https://www.ros.org/) & [**Isaac ROS**](https://developer.nvidia.com/isaac-ros-gems).
-
-<small>Designed & made by [Raffaello Bonghi](https://rnext.it)</small>
-
-[![nanosaur](https://nanosaur.ai/assets/images/banner.jpg)](https://nanosaur.ai)
-
-Meet nanosaur:
-* 🦕 Website: [nanosaur.ai](https://nanosaur.ai)
-* 🦄 Do you need an help? [Discord](https://discord.gg/YvxjxEFPkb)
-* 🧰 For technical details follow [wiki](https://github.com/rnanosaur/nanosaur/wiki)
-* 🐳 nanosaur [Docker hub](https://hub.docker.com/u/nanosaur)
-* ⁉️ Something wrong? Open an [issue](https://github.com/rnanosaur/nanosaur/issues)
+# Isaac ROS - Docker CI
 
 # Build
 
-```
-docker build -t nanosaur/base:latest .
-```
-
-# RUN
+Run
 
 ```
-docker run -it --rm nvidia/cuda:11.4.1-devel-ubuntu20.04 bash
+./build_ros.sh
 ```
 
-```
-docker run -it --rm nanosaur/base:latest bash
-```
+# Images
+
+| Name                        | x86 | ARM64 |
+|-----------------------------|-----|-------|
+| isaac_ros/base:devel        | Yes | Yes   |
+| isaac_ros/base:humble-devel | Yes | Yes   |
+| isaac_ros/base:runtime      | Yes | Yes   |
+| isaac_ros/base:humble       | Yes | Yes   |
--- build_ros.sh
+++ build_ros.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
 # reference
 # https://gitlab.com/nvidia/container-images/samples/-/blob/main/deployments/container/Makefile
@@ -18,17 +38,17 @@ CUDA_VERSION=11.4.1
 
 echo " - ${bold}BUILD DEVEL${reset}"
 
-BASE_IMAGE=nanosaur/base:devel
+BASE_IMAGE=isaac_ros/base:devel
 
 docker build \
-    -t nanosaur/base:devel \
+    -t isaac_ros/base:devel \
     --build-arg BASE_DIST="$BASE_DIST" \
     --build-arg CUDA_VERSION="$CUDA_VERSION" \
     -f Dockerfile.devel \
     .
 
 docker build \
-    -t nanosaur/base:humble-devel \
+    -t isaac_ros/base:humble-devel \
     --build-arg BASE_IMAGE="$BASE_IMAGE" \
     -f Dockerfile.humble \
     .
@@ -37,17 +57,17 @@ docker build \
 
 echo " - ${bold}BUILD RUNTIME${reset}"
 
-BASE_IMAGE=nanosaur/base:runtime
+BASE_IMAGE=isaac_ros/base:runtime
 
 docker build \
-    -t nanosaur/base:runtime \
+    -t isaac_ros/base:runtime \
     --build-arg BASE_DIST="$BASE_DIST" \
     --build-arg CUDA_VERSION="$CUDA_VERSION" \
     -f Dockerfile.runtime \
     .
 
 docker build \
-    -t nanosaur/base:humble \
+    -t isaac_ros/base:humble \
     --build-arg BASE_IMAGE="$BASE_IMAGE" \
     -f Dockerfile.humble \
     .
